fc32b740a5b99e86ab19c646161d9313f395c6e5
Edinorock <111507622+Edinorock@users.noreply.github.com>
2022-09-27 23:03:58 +0300

    Исправлено, методы

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,38 @@
-import java.sql.SQLOutput;
-
 public class Main {
-
-
     public static void main(String[] args) {
-        System.out.println("Задание 1");
-        year = 2021;
-        year();
-        System.out.println("Задание 2");
-        phone();
-        System.out.println("Задание 3");
-        deliveryDistance = 90;
-        bankcardDelivery();
-
+        int year = 2022;
+        checkLeapYear(2022);
+        getDeviceLink(0, 2005);
+        int deliveryDays = calculateDeliveryDays(60);
+        System.out.println("Потребуется дней: " + deliveryDays);
     }
 
-    public static int year;
-
-    public static int deliveryDistance;
-
-    public static void year() {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + " високосный");}
-        else {System.out.println( year + " не високосный");}
-    }
-
-    public static void phone() {
-        int newClientOS = 1;
-        int clientDeviceYear = 2010;
-        boolean newAndroidVersion = (newClientOS == 1 && clientDeviceYear >= 2015);
-        if (newAndroidVersion) {
-            System.out.println("Установите приложение на Android");
-        } else if (newClientOS == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченное приложение на Android");
-        }
-        if (newClientOS == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите приложение на iOS");
-        }
-        if (newClientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченное приложение на iOS");
-
+    public static void checkLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " is leap year");
+        } else {
+            System.out.println(year + " is not leap year");
         }
     }
 
-    public static void bankcardDelivery() {
+    public static void getDeviceLink (int osType, int manufacturingYear) {
+        if (osType != 1 && osType != 0) {
+            throw new IllegalArgumentException("Wrong OS type");
+        }
+        int currentYear = LocalDate.now().getYear();
+        if (manufacturingYear > currentYear) {
+            throw new IllegalArgumentException("Wrong manufacturing year");
+        }
+        String osString = osType == 0 ? "iOS" : "Android";
+        String versionString = manufacturingYear > 2015 ? "полную" : "облегченную";
+        System.out.printf("Установите %s версию для %s по ссылке%n", versionString, osString);
+    }
+
+    public static int calculateDeliveryDays(int deliveryDistance) {
         if (deliveryDistance <= 20) {
-            System.out.println("Потребуется 1 день доставки");
-        }
-        if (deliveryDistance >=20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется 2 суток доставки");
-        }
-        if (deliveryDistance >=60 && deliveryDistance <=100) {
-            System.out.println("Потребуется 3 суток доставки");
+            return 1;
+        } else {
+            return (int) Math.round((double) deliveryDistance / 40) + 1;
         }
     }
 }
